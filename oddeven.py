7a83cb02a3e9fd07ff269d34cf507586dcacafc9
Deepanshu-Bhatt <deepanshu.bhatt@zohomail.in>
2026-01-05 04:18:18 +0530

    Done

--- oddeven.py
+++ oddeven.py
@@ -1,7 +1,13 @@
 #program for Odd & Even num
-def oddeven(num):
+def oddEven(num):
     print("Odd" if num & 1 else "Even")
 
-def decimal():
+def ftd():
     for i in range (1,10):
         print(1/i)
+
+def traversal(seq):
+    for i in seq:
+        print(i,end="\t")
+
+
